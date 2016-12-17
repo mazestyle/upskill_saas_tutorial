80468bc171b84830bf7ff6ab68e7c7d437c7ba88
Petar Petkov <petar.petkov1@outlook.com>
2016-12-17 13:26:14 +0000

    Impored error massages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,11 +6,14 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-       redirect_to new_contact_path, notice: "Message sent."
+      flash[:success] = "Massage sent."
+       redirect_to new_contact_path
     else
-       redirect_to new_contact_path, notice: "Error occured."
+      flash[:danger] = @contact.errors.full_messages.join(", ")
+      redirect_to new_contact_path
     end
   end
+  
   private
     def contact_params
        params.require(:contact).permit(:name, :email, :comments)
